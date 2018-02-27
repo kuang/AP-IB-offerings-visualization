173a74fcd8514fd54b701f8c216f29edfe56ddc5
Justin Kuang <zk65@cornell.edu>
2018-02-27 17:45:22 -0500

    cleaning data, figured out integer parsing issue

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 var svg = d3.select("svg");
 
-// Create a text element to show country names
-// svg.append("text")
-//     .attr("id", "CountryName")
-//     .attr("x", 0)
-//     .attr("y", 300)
-//     .style("font-size", "48pt");
 
 var countryData;
 var rawData, nestedData;
 
 function parseLine(line) {
-    // return { Country: line["Country Name"], Variable: line["Series Name"], value: Number(line["2015 [YR2015]"]) };
-    console.log(line);
-    return line;
+    var remain, enter, leave, fromState, pLeaving;
+    remain = parseInt(line["remaining"]); //not sure why the csv values aren't being parsed as integers directly
+    enter = parseInt(line["entering"]);
+    leave = parseInt(line["leaving"]);
+
+    fromState = remain + leave;
+    console.log(fromState);
+    pLeaving = leave / fromState;
+
+    // var percentLeaving = parseInt(line["remaining"]) + parseInt(line["leaving"]);
+    console.log(line["State"] + "  " + remain + " " + enter + " " + leave + " " + pLeaving);
+    return { State: line["State"] };
+    // , Variable: line["Series Name"], value: Number(line["2015 [YR2015]"]) };
 }
 
 // Some data from http://data.worldbank.org/data-catalog/country-profiles
 
 d3.csv("college_data.csv", parseLine, function (error, data) {
     rawData = data;
-    console.log("loaded data");
+    // console.log(data);
 
 
 });
